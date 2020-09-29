27a62a3ecf8754473e03ceeb64d650c29b9c8f89
19s030 <19s030fxxktion@gmail.com>
2020-09-29 01:11:15 +0000

    change

--- main.py
+++ main.py
@@ -1,45 +1,207 @@
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, FlexSendMessage
 import os
-import template.message_ysdm as ysdm
-import template.message_ymst as ymst
-import template.message_bubble as bubble
+import random
+import json
 
-app=Flask(__name__)
+from pathlib import Path
 
-#環境変数の取得
-YOUR_CHANNEL_ACCESS_TOKEN=os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]="21MB2pzMrEs0JNqAdPTPyxFJmnaljipr9bLiUuMJrPWaLeCPHmK1tnqK23FoVL9kjqnpmyaJ0jFu3/KBCKl+O0WKIYzZ6lqfNEcAGaw3ag8aOwVlNzFsgmgVjiyewGsJOjnlogELVfGGTqz/PRJimwdB04t89/1O/w1cDnyilFU="
-YOUR_CHANNEL_SECRET=os.environ["YOUR_CHANNEL_SECRET"]="b392c7fd703eba783a31c2c6cb80a890"
-line_bot_api=LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler=WebhookHandler(YOUR_CHANNEL_SECRET)
+from flask import Flask, abort, request
+from linebot import (
+   LineBotApi, WebhookHandler
+)
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import (ImageMessage, ImageSendMessage, MessageEvent,
+                            TextMessage, TextSendMessage, FlexSendMessage)
+
+import cv2
+
+
+
+
+
+app = Flask(__name__)
+
+
+face_cascade_path = "haarcascade_frontalface_default.xml"
+eye_cascade_path = "haarcascade_eye.xml"
+
+YOUR_CHANNEL_ACCESS_TOKEN = "21MB2pzMrEs0JNqAdPTPyxFJmnaljipr9bLiUuMJrPWaLeCPHmK1tnqK23FoVL9kjqnpmyaJ0jFu3/KBCKl+O0WKIYzZ6lqfNEcAGaw3ag8aOwVlNzFsgmgVjiyewGsJOjnlogELVfGGTqz/PRJimwdB04t89/1O/w1cDnyilFU="
+YOUR_CHANNEL_SECRET = "b392c7fd703eba783a31c2c6cb80a890"
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+#SRC_IMAGE_PATH = "https://secret-lake-56663.herokuapp.com/static/{}.jpg"
+#MAIN_IMAGE_PATH = "https://secret-lake-56663.herokuapp.com/static/{}_main.jpg"
+#PREVIEW_IMAGE_PATH = "https://secret-lake-56663.herokuapp.com/static/{}_preview.jpg"
+
+@app.route("/")
+def hello_world():
+    return "hello world!"
+
 
 @app.route("/callback", methods=["POST"])
 def callback():
-    signature = request.headers["X-Line-Signature"]
+    # get X-Line-Signature header value
+    signature = request.headers["X-Line-Signature"]     #lineしか受け付けない
 
+    # get request body as text
     body = request.get_data(as_text=True)
-    print("Request body: {}".format(body))
+    app.logger.info("Request body: " + body)
 
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return "OK"
 
+
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    contents = []
-    #flex_ysdm = FlexSendMessage.new_from_json_dict(ysdm.get_ysdm_message())
-    flex_ymst = FlexSendMessage.new_from_json_dict(ymst.get_ymst_message())
-    #flex_bubble = FlexSendMessage.new_from_json_dict(bubble.get_message_bubble())
-    #contents.append(flex_ysdm)
-    contents.append(flex_ymst)
-    #contents.append(flex_bubble)
-    #line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
-    line_bot_api.reply_message(event.reply_token, messages=contents)
+    number =  random.randint(0,3)
+    if number == 0:
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="大吉")
+        #TextSendMessage(text=event.message.text)
+    )
+    elif number == 1:
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="中吉")
+    )
+    elif number == 2:
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="吉")
+    )
+    else:
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="凶")
+    )
+    
+    
 
-if __name__=="__main__":
-    port = int(os.getenv("PORT",5000))
+def handle_textmessage(event):
+    print("ログ成功！！！！！！！！！")
+    line_bot_api.reply_message(
+        event.reply_token,
+        [
+            TextSendMessage(text="目を検知できませんでした。"),
+            TextSendMessage(text="他の画像を送信してください。"),
+        ]
+    )
+
+
+
+
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image(event):
+    message_id = event.message.id
+
+    # 画像を保存
+
+    message_content = line_bot_api.get_message_content(message_id)
+
+    with open("static/"+ message_id + ".jpg", "wb") as f:
+        f.write(message_content.content)
+
+    #with open('test.json') as f:
+    #    df = json.load(f)
+    payload = {
+        "type": "template",
+        "altText": "this is a carousel template",
+        "template": {
+          "type": "carousel",
+          "actions": [],
+          "columns": [
+            {
+              "thumbnailImageUrl": "SPECIFY_YOUR_IMAGE_URL",
+              "title": "加工処理",
+              "text": "加工したい処理を選択してください",
+              "actions": [
+                {
+                  "type": "message",
+                  "label": "髪加工",
+                  "text": "1"
+                },
+                {
+                  "type": "message",
+                  "label": "目元加工",
+                  "text": "2"
+                }
+            ]
+        }
+    ]
+  }
+}
+
+    container_obj = FlexSendMessage.new_from_json_dict(payload)
+
+    line_bot_api.push_message('送りたい相手のUserID', messages=container_obj)
+
+    result = change_image(event)
+    if result:
+        print("ログ成功！！！！！！！！！")
+        line_bot_api.reply_message(
+            event.reply_token, ImageSendMessage(
+                original_content_url="https://team-hagi-project.herokuapp.com/static/mosaic.jpg",
+                preview_image_url="https://team-hagi-project.herokuapp.com/static/mosaic.jpg",
+            )
+        )
+    else:
+        handle_textmessage(event)
+
+def change_image(event):
+    message_id = event.message.id
+    fname = "static/" + message_id + ".jpg"  # 画像ファイル名
+
+
+
+    eye_cascade = cv2.CascadeClassifier(eye_cascade_path)
+
+    src = cv2.imread(fname)
+    src_mosaic = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+
+    eyes = eye_cascade.detectMultiScale(src_mosaic)
+
+    ratio = 0.05     #縮小処理時の縮小率(小さいほどモザイクが大きくなる)
+
+
+# 検出した場合
+    #if len(facerect) > 0:
+#
+    #    # 検出した顔を囲む矩形の作成
+    #    for rect in facerect:
+    #        cv2.rectangle(image, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=2)
+    #else:
+    #    return False
+#
+    #cv2.imwrite(output_path, image)
+    ## 認識結果の保存
+
+    if len(eyes) > 0:
+        for x, y, w, h in eyes:  # 引数でeyesで取得した数分forループ
+            # y:はHEIGHT、x:はWEIGHT  fxはxの縮小率、fyはyの縮小率
+            small = cv2.resize(src[y: y + h, x: x + w], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
+            src[y: y + h, x: x + w] = cv2.resize(small, (w, h), interpolation=cv2.INTER_NEAREST)
+    else:
+        return False
+
+    cv2.imwrite("static/mosaic.jpg", src)
+
+    return True
+#def save_image(message_id: str, save_path: str) -> None:
+    #"""保存"""
+    #message_content = line_bot_api.get_message_content(message_id)
+    #with open(save_path, "wb") as f:
+       # for chunk in message_content.iter_content():
+        #    f.write(chunk)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
