f46c3beae0e516661ff1954092b4a6e253c853b1
19s030 <19s030fxxktion@gmail.com>
2020-10-06 02:04:38 +0000

    change

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSend
 import json
 import os
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
 work = {}
 path_w1 = 'saveid.txt'
 path_w2 = 'savereply.txt'
@@ -46,6 +49,7 @@ def handle_message(event):
     print("リプライトークン：{}".format(event.reply_token))
     print("------リプライ型------")
     print(type(event.reply_token))
+    #モザイク(目)
     if event.message.text == "1":
         print("通過: {}".format(event.message.text))
         with open(path_w1) as f:
@@ -53,6 +57,15 @@ def handle_message(event):
         with open(path_w2) as f2:
             work1 = f2.read()
         handle_send_message2(work,work1)
+    #線画
+    elif event.message.text == "2":
+        print("通過: {}".format(event.message.text))
+        with open(path_w1) as f:
+            work = f.read()
+        with open(path_w2) as f2:
+            work1 = f2.read()
+        handle_send_message3(work,work1)
+
 
 
 
@@ -126,21 +139,21 @@ def handle_image_message(event):
     
 
     
-#画像送信処理
-def handle_send_message(event):
-    #mozaiku(event)
-    result = change_image(event)
+# #画像送信処理
+# def handle_send_message(event):
+#     mozaiku(event)
+#     result = change_image(event)
 
-    if result:
-        line_bot_api.reply_message(
-            event.reply_token, ImageSendMessage(
-                original_content_url=FQDN + "/static/" + event.message.id + "_face.jpg",
-                preview_image_url=FQDN + "/static/" + event.message.id + "_face.jpg",
-            )
-            )
+#     if result:
+#         line_bot_api.reply_message(
+#             event.reply_token, ImageSendMessage(
+#                 original_content_url=FQDN + "/static/" + event.message.id + "_face.jpg",
+#                 preview_image_url=FQDN + "/static/" + event.message.id + "_face.jpg",
+#             )
+#             )
 
-    else:
-        handle_textmessage(event)
+#     else:
+#         handle_textmessage(event)
 
 
 #画像送信処理
@@ -159,7 +172,25 @@ def handle_send_message2(event,relpy):
     else:
         handle_textmessage(event)
 
-
+def handle_send_message3(event,reply):
+    result = art_image(event)
+    reply = str(reply)
+    if result:
+        line_bot_api.reply_message(
+            reply, ImageSendMessage(
+                original_content_url=FQDN + "/static/" + event + "_face.jpg",
+                preview_image_url=FQDN + "/static/" + event + "_face.jpg",
+            )
+            )
+    else:
+        handle_textmessage(event)
+    
+    # plt.close("all")
+    # plt.figure(figsize=[8, 8])
+    plt.set_cmap("gray")
+    # plt.clf()
+    art_image(event)
+    
 #囲う処理
 def change_image(event):
     bool = True
@@ -232,6 +263,43 @@ def change_image(event):
     else:
         return False
 
+def art_image(event):
+    image_file = event + ".jpg"
+    save_file = event + "_face.jpg"
+    #save_file2 = event.message.id + "_face2.jpg"
+    print("イメージファイル: {} // {}".format(image_file, save_file))
+    image_path = "static/" + image_file
+    print("イメージパス: {}".format(image_path))
+    output_path = "static/" + save_file
+    #output_path2 = "static/" + save_file2
+    print("アウトプットパス: {}".format(output_path))
+    # カーネルを定義
+    kernel = np.ones((5,5), np.uint8)
+    kernel[0,0] = kernel[0,4] = kernel[4,0] = kernel[4,4] = 0
+    # グレースケールで画像を読み込む.
+    # gray = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    # いらすとやの画像はアルファチャンネルがあるのでこれをまず白にする
+    # ImageMagickの convert -flatten x.png y.png に対応
+    img = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+    if img.shape[2] == 4:
+        mask = img[:,:,3] == 0
+        img[mask] = [255] * 4
+        img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # ノイズ除去が必要なら
+    # gray = cv2.fastNlMeansDenoising(gray, h=20)
+    # 白い部分を膨張させる
+    dilated = cv2.dilate(gray, kernel, iterations=1)
+    # 差をとる
+    diff = cv2.absdiff(dilated, gray)
+    # 白黒反転して2値化
+    # _, contour = cv2.threshold(255 - diff, 240, 255, cv2.THRESH_BINARY)
+    # あるいは
+    image = cv2.adaptiveThreshold(255 - diff, 255,
+                                    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                    cv2.THRESH_BINARY, 7, 8)
+
+    cv2.imwrite(output_path, image)
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
