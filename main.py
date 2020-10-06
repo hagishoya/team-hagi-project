8b8d1e21f49374e500bb9738e852f63622f80106
19s030 <19s030fxxktion@gmail.com>
2020-10-06 01:14:20 +0000

    change

--- main.py
+++ main.py
@@ -1,56 +1,29 @@
-import os
-import random
-import json 
-
-from pathlib import Path
-
-from flask import Flask, abort, request
-from linebot import (
-   LineBotApi, WebhookHandler
-)
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import (ImageMessage, ImageSendMessage, MessageEvent,
-                            TextMessage, TextSendMessage, FlexSendMessage)
-
-import numpy as np
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, ImageMessage, FlexSendMessage,CarouselContainer,BubbleContainer
+import json
+import os
 import cv2
-import matplotlib.pyplot as plt
-
-
-
-
-
+work = {}
+path_w1 = 'saveid.txt'
+path_w2 = 'savereply.txt'
 app = Flask(__name__)
-
-
-face_cascade_path = "haarcascade_frontalface_default.xml"
-eye_cascade_path = "haarcascade_eye.xml"
 
 YOUR_CHANNEL_ACCESS_TOKEN = "21MB2pzMrEs0JNqAdPTPyxFJmnaljipr9bLiUuMJrPWaLeCPHmK1tnqK23FoVL9kjqnpmyaJ0jFu3/KBCKl+O0WKIYzZ6lqfNEcAGaw3ag8aOwVlNzFsgmgVjiyewGsJOjnlogELVfGGTqz/PRJimwdB04t89/1O/w1cDnyilFU="
 YOUR_CHANNEL_SECRET = "b392c7fd703eba783a31c2c6cb80a890"
-
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
-
-#SRC_IMAGE_PATH = "https://secret-lake-56663.herokuapp.com/static/{}.jpg"
-#MAIN_IMAGE_PATH = "https://secret-lake-56663.herokuapp.com/static/{}_main.jpg"
-#PREVIEW_IMAGE_PATH = "https://secret-lake-56663.herokuapp.com/static/{}_preview.jpg"
-
-@app.route("/")
-def hello_world():
-    return "hello world!"
+FQDN = " https://team-hagi-project.herokuapp.com"
 
 
 @app.route("/callback", methods=["POST"])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers["X-Line-Signature"]     #lineしか受け付けない
+    signature = request.headers["X-Line-Signature"]
 
-    # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    print("Request body" + body)
 
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -58,196 +31,208 @@ def callback():
     return "OK"
 
 
-
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    number =  random.randint(0,3)
-    if number == 0:
-        line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="大吉")
-        #TextSendMessage(text=event.message.text)
-    )
-    elif number == 1:
-        line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="中吉")
-    )
-    elif number == 2:
-        line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="吉")
-    )
-    else:
-        line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="凶")
-    )
-    
-    
-
-def handle_textmessage(event):
-    print("ログ成功！！！！！！！！！")
-    line_bot_api.reply_message(
-        event.reply_token,
-        [
-            TextSendMessage(text="目を検知できませんでした。"),
-            TextSendMessage(text="他の画像を送信してください。"),
-        ]
-    )
+    line_bot_api.reply_message(event.reply_token,
+       [
+           TextSendMessage(text=event.message.text),
+           #TextSendMessage(text="おはよ------"),
+           #TextSendMessage(text="顔、目を検知できませんでした。"),
+           #TextSendMessage(text=event.message.id),
+       ]
+       )
+    print("取得イヴェント:{}".format(event))
+    print("取得イヴェントメッセージID:{}".format(event.message.id))
+    print("リプライトークン：{}".format(event.reply_token))
+    print("------リプライ型------")
+    print(type(event.reply_token))
+    if event.message.text == "1":
+        print("通過: {}".format(event.message.text))
+        with open(path_w1) as f:
+            work = f.read()
+        with open(path_w2) as f2:
+            work1 = f2.read()
+        handle_send_message2(work,work1)
 
 
 
+def text_save_id(work):
+    s = work
+    print("取得イヴェントメッセージIDDDDDDDDDDDDDDDD_text_saveID:{}".format(work))
+    with open(path_w1, mode='w') as f:
+        f.write(s)
 
-@handler.add(MessageEvent, message=ImageMessage)
-def handle_image(event):
-    message_id = event.message.id
-
-    # 画像を保存
-
-    message_content = line_bot_api.get_message_content(message_id)
-
-    with open("static/"+ message_id + ".jpg", "wb") as f:
-        f.write(message_content.content)
-
-    flex_result = flex_message(event)
-
-    
-    change_image2(event)
-    #if result:
-
-    line_bot_api.reply_message(
-        event.reply_token, ImageSendMessage(
-            original_content_url="https://team-hagi-project.herokuapp.com/static/mosaic.jpg",
-            preview_image_url="https://team-hagi-project.herokuapp.com/static/mosaic.jpg",
-        )
-    )
-    #else:
-    handle_textmessage(event)
- def art_image(event):   
-# カーネルを定義
-    kernel = np.ones((5,5), np.uint8)
-    kernel[0,0] = kernel[0,4] = kernel[4,0] = kernel[4,4] = 0
-    # グレースケールで画像を読み込む.
-    # gray = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    # いらすとやの画像はアルファチャンネルがあるのでこれをまず白にする
-    # ImageMagickの convert -flatten x.png y.png に対応
-    img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-    if img.shape[2] == 4:
-        mask = img[:,:,3] == 0
-        img[mask] = [255] * 4
-        img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # ノイズ除去が必要なら
-    # gray = cv2.fastNlMeansDenoising(gray, h=20)
-    # 白い部分を膨張させる
-    dilated = cv2.dilate(gray, kernel, iterations=1)
-    # 差をとる
-    diff = cv2.absdiff(dilated, gray)
-    # 白黒反転して2値化
-    # _, contour = cv2.threshold(255 - diff, 240, 255, cv2.THRESH_BINARY)
-    # あるいは
-    contour = cv2.adaptiveThreshold(255 - diff, 255,
-                                    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                    cv2.THRESH_BINARY, 7, 8)
-    return contour
-    # plt.close("all")
-    # plt.figure(figsize=[8, 8])
-    plt.set_cmap("gray")
-    # plt.clf()
-    contour = makecontour("bug_chou_tawamureru_kids.png")
-    plt.imshow(contour)
-    cv2.imwrite("190831a.png", contour)
+def text_save_reply(work):
+    s = work
+    print("取得イヴェントメッセージIDDDDDDDDDDDDDDDD_text_saveReply:{}".format(work))
+    with open(path_w2, mode='w') as f:
+        f.write(s)
 
 
-def flex_message(event):
-    f = open('test.json', 'r')
-    messages = json.load(f)
-    messages = FlexSendMessage(alt_text="test", contents=messages)
+def flex(event):
+    work = event.message.id
+    reply_work = event.reply_token
+    print("取得イヴェントメッセージIDDDDDDDDDDDDDDDD:{}".format(work))
+    text_save_id(work)
+    text_save_reply(reply_work)
+    json_open = open('hello.json', 'r')
+    json_data = json.load(json_open)
+    #print("json_data: {}".format(json_data.get("hero").get("url")))
+    #print(json_data.get("hero").get("url"))
+    #json_data["hero"]
+    #message = line_bot_api.reply_message(
+    #    event.reply_token,
+    #    [
+    #        FlexSendMessage(
+    #        alt_text="flex",
+    #        contents=BubbleContainer.new_from_json_dict(json_data)
+    #        )
+    #    ]
+    #)
 
-
-    
+    messages = FlexSendMessage(alt_text="test", contents=json_data)
+    print("フレックスメッセージ中身: {}".format(messages))
     if event.reply_token == "00000000000000000000000000000000":
         return
-
     if event.reply_token == "ffffffffffffffffffffffffffffffff":
         return
+        
+    line_bot_api.push_message('U0702a57cd35b16d81966cf38edfecb78', messages=messages)
 
-    user_id = "U0702a57cd35b16d81966cf38edfecb78"
-    line_bot_api.push_message(user_id, messages=messages)
+
+def handle_textmessage(event):
+    line_bot_api.reply_message(event.reply_token,
+        [
+            #TextSendMessage(text=event.message.text),
+            TextSendMessage(text="顔、目を検知できませんでした。"),
+            #TextSendMessage(text=event.message.id),
+        ]
+        )
+
+#画像受信後処理
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image_message(event):
+    print("メッセージID")
+    print(event.message.id)
+    message_content = line_bot_api.get_message_content(event.message.id)
+    if not os.path.exists('static'):
+        os.mkdir('static/')
+    with open("static/" + event.message.id + ".jpg", "wb") as f:
+        f.write(message_content.content)
+    
+
+    flex(event)
+
+    
+
+    
+#画像送信処理
+def handle_send_message(event):
+    #mozaiku(event)
+    result = change_image(event)
+
+    if result:
+        line_bot_api.reply_message(
+            event.reply_token, ImageSendMessage(
+                original_content_url=FQDN + "/static/" + event.message.id + "_face.jpg",
+                preview_image_url=FQDN + "/static/" + event.message.id + "_face.jpg",
+            )
+            )
+
+    else:
+        handle_textmessage(event)
 
 
-    return messages
+#画像送信処理
+def handle_send_message2(event,relpy):
+    #mozaiku(event)
+    result = change_image(event)
+    reply = str(relpy)
+    if result:
+        line_bot_api.reply_message(
+            reply, ImageSendMessage(
+                original_content_url=FQDN + "/static/" + event + "_face.jpg",
+                preview_image_url=FQDN + "/static/" + event + "_face.jpg",
+            )
+            )
 
+    else:
+        handle_textmessage(event)
+
+
+#囲う処理
 def change_image(event):
-    message_id = event.message.id
-    fname = "static/" + message_id + ".jpg"  # 画像ファイル名
+    bool = True
+    cascade_path = "haarcascade_frontalface_default.xml"
+    cascade_eye_path = "haarcascade_eye.xml"
 
+    image_file = event + ".jpg"
+    save_file = event + "_face.jpg"
+    #save_file2 = event.message.id + "_face2.jpg"
+    print("イメージファイル: {} // {}".format(image_file, save_file))
+    image_path = "static/" + image_file
+    print("イメージパス: {}".format(image_path))
+    output_path = "static/" + save_file
+    #output_path2 = "static/" + save_file2
+    print("アウトプットパス: {}".format(output_path))
+    # ファイル読み込みo
+    image = cv2.imread(image_path)
 
+    # グレースケール変換
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    eye_cascade = cv2.CascadeClassifier(eye_cascade_path)
+    # カスケード分類器の特徴量を取得する
+    cascade = cv2.CascadeClassifier(cascade_path)
+    cascade_eye = cv2.CascadeClassifier(cascade_eye_path)
+    # 物体認識（顔認識）の実行
+    # image – CV_8U 型の行列．ここに格納されている画像中から物体が検出されます
+    # objects – 矩形を要素とするベクトル．それぞれの矩形は，検出した物体を含みます
+    # scaleFactor – 各画像スケールにおける縮小量を表します
+    # minNeighbors – 物体候補となる矩形は，最低でもこの数だけの近傍矩形を含む必要があります
+    # flags – このパラメータは，新しいカスケードでは利用されません．古いカスケードに対しては，cvHaarDetectObjects 関数の場合と同じ意味を持ちます
+    # minSize – 物体が取り得る最小サイズ．これよりも小さい物体は無視されます
+    facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=2, minSize=(30, 30))
+    eyerect = cascade_eye.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=2, minSize=(20, 20))
+    print("レクト:{} // {}".format(facerect, eyerect))
+    print("ぎっとぽっど")
 
-    src = cv2.imread(fname)
-    src_mosaic = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    color = (255, 0, 0)  # 青
 
-    eyes = eye_cascade.detectMultiScale(src_mosaic)
-
-    ratio = 0.05     #縮小処理時の縮小率(小さいほどモザイクが大きくなる)
-
-
-# 検出した場合
+    ## 検出した場合
     #if len(facerect) > 0:
-#
     #    # 検出した顔を囲む矩形の作成
     #    for rect in facerect:
-    #        cv2.rectangle(image, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=2)
+    #        cv2.rectangle(image, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=1)
+    #        print(facerect)
+    #        print(rect)
     #else:
-    #    return False
-#
-    #cv2.imwrite(output_path, image)
-    ## 認識結果の保存
+    #    bool = False
 
-    if len(eyes) > 0:
-        for x, y, w, h in eyes:  # 引数でeyesで取得した数分forループ
-            # y:はHEIGHT、x:はWEIGHT  fxはxの縮小率、fyはyの縮小率
-            small = cv2.resize(src[y: y + h, x: x + w], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
-            src[y: y + h, x: x + w] = cv2.resize(small, (w, h), interpolation=cv2.INTER_NEAREST)
+    ratio = 0.05  # 縮小処理時の縮小率(小さいほどモザイクが大きくなる)
+    if len(eyerect) > 0:
+        # for rect in eyerect:
+        #     cv2.rectangle(image, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=1)
+        #     print(eyerect)
+        #     print(rect)
+           
+
+        for x, y, w, h in eyerect:  # 引数でeyesで取得した数分forループ
+           # y:はHEIGHT、x:はWEIGHT  fxはxの縮小率、fyはyの縮小率
+           small = cv2.resize(image[y: y + h, x: x + w], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
+           image[y: y + h, x: x + w] = cv2.resize(small, (w, h), interpolation=cv2.INTER_NEAREST)
+           
+    else:
+        bool = False
+
+    if bool:
+        # 認識結果の保存
+        cv2.imwrite(output_path, image)
+        #cv2.imwrite(output_path2, image)
+        return True
     else:
         return False
 
-    cv2.imwrite("static/mosaic.jpg", src)
 
-    return True
-#def save_image(message_id: str, save_path: str) -> None:
-    #"""保存"""
-    #message_content = line_bot_api.get_message_content(message_id)
-    #with open(save_path, "wb") as f:
-       # for chunk in message_content.iter_content():
-        #    f.write(chunk)
-
-
-def change_image2(event):
-    message_id = event.message.id
-    fname = "static/" + message_id + ".jpg"  # 画像ファイル名
-    img = cv2.imread(fname)
-
-
-    height = img.shape[0]
-    width = img.shape[1]
-
-    img2 = cv2.resize(img , (int(width*0.5), int(height*0.5)))
-    hsv = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV) # BGR->HSV変換
-    hsv_2 = np.copy(hsv)
-    hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>16) & (hsv[:, :, 0]<25) ,hsv[:, :,(2)]*0.2,hsv[:, :, 0])
-    bgr = cv2.cvtColor(hsv_2, cv2.COLOR_HSV2BGR)
-    #cv2.imshow('image',bgr)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-
-    cv2.imwrite("static/mosaic.jpg", bgr)
-    cv2.waitKey(0)
-#https://teratail.com/questions/284276
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
