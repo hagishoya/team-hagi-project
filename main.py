ef47442586c85ca6a1c305f0b55fd8c9414849bc
19s030 <19s030fxxktion@gmail.com>
2020-10-13 01:36:38 +0000

    cng

--- main.py
+++ main.py
@@ -203,15 +203,7 @@ def handle_send_message4(event,relpy):
         )
         )
 
-
-################################################################
-###-------------------------モザイク--------------------------###
-
-#囲う処理
-def change_image(event):
-    bool = True
-    cascade_path = "haarcascade_frontalface_default.xml"
-    cascade_eye_path = "haarcascade_eye.xml"
+def get_image_path(event):
 
     image_file = event + ".jpg"
     save_file = event + "_face.jpg"
@@ -224,6 +216,34 @@ def change_image(event):
     # 加工済みの画像の保存場所をパスとして保管
     output_path = "static/" + save_file
     print("アウトプットパス: {}".format(output_path))
+
+    return image_path, output_path
+
+
+
+################################################################
+###-------------------------モザイク--------------------------###
+
+#囲う処理
+def change_image(event):
+    bool = True
+    cascade_path = "haarcascade_frontalface_default.xml"
+    cascade_eye_path = "haarcascade_eye.xml"
+
+    # image_file = event + ".jpg"
+    # save_file = event + "_face.jpg"
+    # print("イメージファイル: {} // {}".format(image_file, save_file))
+
+    # # 元画像の保存場所をパスとして保管
+    # image_path = "static/" + image_file
+    # print("イメージパス: {}".format(image_path))
+
+    # # 加工済みの画像の保存場所をパスとして保管
+    # output_path = "static/" + save_file
+    # print("アウトプットパス: {}".format(output_path))
+
+    image_path, output_path = get_image_path(event)
+
     # ファイル読み込みo
     image = cv2.imread(image_path)
 
