4c1b1d72cf07b2ba78565c06f6c3688dfdbd43c6
19s030 <19s030fxxktion@gmail.com>
2020-10-13 05:14:42 +0000

    sss

--- output/output_method.py
+++ output/output_method.py
@@ -26,7 +26,7 @@ def handle_send_message(event,relpy):
             )
             )
     print("check result:{}".format(event))
-    main.result_message(reply)
+    main.result_message(event)
     
 
     # else:
@@ -68,5 +68,4 @@ def handle_send_message4(event,relpy):
             preview_image_url=main.FQDN + "/static/" + event + "_face.jpg",
         )
         )
-    main.result_message(reply)
 ################################################################
