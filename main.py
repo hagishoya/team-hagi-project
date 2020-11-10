aa407749887585b7aa337ef8e8f58ec758f7e3b4
19s030 <19s030fxxktion@gmail.com>
2020-11-10 05:28:38 +0000

    c

--- main.py
+++ main.py
@@ -138,11 +138,13 @@ def handle_image_message(event):
 
     message_content = line_bot_api.get_message_content(event.message.id)
 
-    if not os.path.exists('static'):
-        os.mkdir('static/')
-    with open("static/" + event.message.id + ".jpg", "wb") as f:
+    if not os.path.exists(event.source.userId):
+        os.mkdir(event.source.userId + '/')
+    with open(event.source.userId + "/" + event.message.id + ".jpg", "wb") as f:
         f.write(message_content.content)
-    
+    print("userId確認用:{}".format(event.source.userId))
+
+
     carousel(event)
 
 
--- path_data.py
+++ path_data.py
@@ -17,11 +17,11 @@ def get_image_path(event):
     print("イメージファイル: {} // {}".format(image_file, save_file))
 
     # 元画像の保存場所をパスとして保管
-    image_path = "static/" + image_file
+    image_path = event.source.userId + '/' + image_file
     print("イメージパス: {}".format(image_path))
 
     # 加工済みの画像の保存場所をパスとして保管
-    output_path = "static/" + save_file
+    output_path = event.source.userId + '/' + save_file
     print("アウトプットパス: {}".format(output_path))
 
     return image_path, output_path
