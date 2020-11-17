e7679b30065f3cdc9238f59440156f9552442fc2
19s030 <19s030fxxktion@gmail.com>
2020-11-17 03:12:48 +0000

    s

--- path_data.py
+++ path_data.py
@@ -18,11 +18,11 @@ def get_image_path(event,userid):
     print("イメージファイル: {} // {}".format(image_file, save_file))
 
     # 元画像の保存場所をパスとして保管
-    image_path = userid + "/" + image_file
+    image_path = "static/" + userid + "/" + image_file
     print("イメージパス: {}".format(image_path))
 
     # 加工済みの画像の保存場所をパスとして保管
-    output_path = userid + "/" + save_file
+    output_path ="static/" + userid + "/" + save_file
     print("アウトプットパス: {}".format(output_path))
 
     return image_path, output_path
