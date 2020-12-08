66174eecf737a8345a27329e5e69da97fc32dbf3
19s030 <19s030fxxktion@gmail.com>
2020-12-08 02:55:43 +0000

    ss

--- image_change/hair_change.py
+++ image_change/hair_change.py
@@ -57,13 +57,35 @@ def is_bold(pnt, hair_mask):
         print("Not Bold")
         return False
 
-def hair_image(event,userid):
+#def hsv_to_rgb(h,s,v):
+##    h = 0 ~ 360
+##    s = 0 ~ 255
+##    v = 0 ~ 255
+#    i = int(h / 60.0)
+#    mx = v
+#    mn = v - ((s / 255.0) * v)
+#    if h is None:
+#        return(0,0,0)
+#    if i == 0:
+#        (r1,g1,b1) = (mx,(h/60.0)*(mx-mn)+mn,mn)
+#    elif i == 1:
+#        (r1,g1,b1) = (((120.0-h)/60.0)*(mx-mn)+mn,mx,mn)
+#    elif i == 2:
+#        (r1,g1,b1) = (mn,mx,((h-120.0)/60.0)*(mx-mn)+mn)
+#    elif i == 3:
+#        (r1,g1,b1) = (mn,((240.0-h)/60.0)*(mx-mn)+mn,mx)
+#    elif i == 4:
+#        (r1,g1,b1) = (((h-240.0)/60.0)*(mx-mn)+mn,mn,mx)
+#    elif 5 <= i:
+#        (r1,g1,b1) = (mx,mn,((360.0-h)/60.0)*(mx-mn)+mn)
+#    return (int(r1), int(g1), int(b1))
 
+def hair_image(event,userid):
     image_path, output_path = path_data.get_image_path(event,userid)
 
     image = cv2.imread(image_path)     # Load image
     image = imutils.resize(image, height=500)     # We result in 500px in height
-    mask = get_head_mask(image)      # We get the mask of the head (without BG)
+    mask = get_head_mask(image)      # We get the mask of the head (without BG))
 
     # Find the contours, take the largest one and memorize its upper point as the top of the head
     cnts = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[0]
@@ -84,25 +106,49 @@ def hair_image(event,userid):
     i1 = cv2.bitwise_and(image, image, mask=mask)
 
 
-    # If the head is bald, then we deduce that the bald head shows the coordinates of the top point of the head
+    # 髪の毛なし
     if is_bold(topmost,mask):
         cv2.rectangle(image,topmost,topmost,(0,0,255),5)
         print(topmost)
 
-    # Otherwise we write that we are not bald and display the coordinates of the largest contour
+
+
+    # 髪の毛あり
     else:
+        #輪郭取得
         cnts = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
         cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
         cv2.drawContours(image,[cnts[0]],-1,(0,0,255),2)
-        cv2.fillPoly(image, pts =[cnts[0]], color=(255, 0, 0))
+       # green = np.uint8([[[0,255,0 ]]])
+        #bgr = cv2.cvtColor(np.array([[[0,255,0]]], dtype=np.uint8), cv2.COLOR_HSV2BGR)[0][0]
+        #ポリゴンの領域を塗りつぶす
         
+        #test_color = hsv_to_rgb(300, 200 , 200)
+        # 色基準で2値化する。
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        # 色の範囲を指定する
+        lower_color = np.array([0, 0, 0])
+        upper_color = np.array([255, 255, 255])
+
+        # 指定した色に基づいたマスク画像の生成
+        mask = cv2.inRange(hsv, lower_color, upper_color)
+        output = cv2.bitwise_and(hsv, hsv, mask = mask)
+        print(output)
+        print("\n\n\n\n\n\n\n\n\n")
+        bgr_output = colorsys.hsv_to_rgb(output)
+        bgr_color = bgr_output + (0,0,80)
+        cv2.fillPoly(image, pts =[cnts[0]], color= bgr_color)
+        #cv2.fillPoly(image, pts =[cnts[0]], color= (255,0,0))
+        #green = np.uint8([[[0,255,0 ]]])
+        #hsv_green = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
+        #image[:] = hsv_green
         for c in cnts[0]:
             print(c)
 
 
     if bool:
         # 認識結果の保存
-        #hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         cv2.imwrite(output_path, image)
         #cv2.imwrite(output_path2, image)
         return True
