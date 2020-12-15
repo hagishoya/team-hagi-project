e42d870ea3238055fe4887debaab2b2b6505a7c9
hagishoya <hagi19s022@gmail.com>
2020-12-15 06:14:07 +0000

    xyhair

--- main.py
+++ main.py
@@ -422,10 +422,10 @@ def change_image2(event):
     print("xyhair:{}".format(xyhair))
     print("xhair:{}".format(xyhair[0]))
     print("yhair:{}".format(xyhair[1]))
-    x = np.array(xyhair)
-    print("xhair{}".format(x))
-    #for i in xyhair: 
-    #    print(xyhair[i][i])
+    
+    for i in xyhair: 
+        xyhair = xyhair[0][i],xyhair[1][i]
+    print("forxyhair:{}".format(xyhair))
     #----------------------------------------------------------------------------------
     im = Image.new('RGB', (500, 250), (128, 128, 128))
     draw = ImageDraw.Draw(im)
