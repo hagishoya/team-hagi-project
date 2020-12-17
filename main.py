12cd852687681e6dfe8a36a4d10655a4bbda770c
hagishoya <hagi19s022@gmail.com>
2020-12-17 01:34:54 +0000

    a

--- main.py
+++ main.py
@@ -444,7 +444,7 @@ def change_image2(event):
     # plot an image with only the hair's cluster on a white background
     #白い背景に髪の毛のクラスターのみを含む画像をプロットします
     cv2.imwrite(output_path, np.where(hairmask[..., None], img1, [255,255,255]))
-    cv2.imwrite(output_path, output1)
+    #cv2.imwrite(output_path, output1)
     return True
     #接続されているすべてのブロブにヘアマスクでラベルを付ける
     #bloblab = snd.label(hairmask, structure=np.ones((3,3)))[0]
