8531022d152a8c2c720560a2f88502b90022052d
hagishoya <hagi19s022@gmail.com>
2021-01-12 03:05:19 +0000

    draw_contours_B

--- main.py
+++ main.py
@@ -342,7 +342,24 @@ def hsv2rgb(h,s,v):
     return (round(i * 255) for i in colorsys.hsv_to_rgb(h,s,v))
 
 
-
+def draw_contours_B(output_path, image_path):
+    # 2値化
+    gray = cv2.cvtColor(image_path, cv2.COLOR_BGR2GRAY)
+    #cv2.imwrite(now + '_1_' + file_name + '_gray_B.jpg', gray)
+    
+    ret,th1 = cv2.threshold(gray,200,255,cv2.THRESH_BINARY)
+    #cv2.imwrite(now + '_2_' + file_name + '_th1_B.jpg', gray)
+    # 輪郭抽出
+    contours, hierarchy = cv2.findContours(th1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    areas = []
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 10000:
+            epsilon = 0.1*cv2.arcLength(cnt,True)
+            approx = cv2.approxPolyDP(cnt,epsilon,True)
+            areas.append(approx)
+    #cv2.drawContours(img, areas, -1, (0,255,0), 3)
+    cv2.imwrite(output_path, image_path)
 
 
 def change_image2(event):
@@ -355,6 +372,8 @@ def change_image2(event):
     output_path = "static/" + save_file
     #output_path2 = "static/" + save_file2
     print("アウトプットパス: {}".format(output_path))
+
+    draw_contours_B(output_path, image_path)
 
     img = cv2.imread(image_path)     # Load image
     height = img.shape[0]
