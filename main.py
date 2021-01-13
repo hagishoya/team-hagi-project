7b52fa77ee0b3af2dda67d45d98d03a224fd90b3
hagishoya <hagi19s022@gmail.com>
2021-01-13 04:58:07 +0000

    img変更

--- main.py
+++ main.py
@@ -406,7 +406,7 @@ def change_image2(event):
 
 #----------------------------------------------------------------------------------
     # Convert BGR to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # define range of blue color in HSV
     lower_blue = np.array([110,50,50])
@@ -416,7 +416,7 @@ def change_image2(event):
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
     # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(frame,frame, mask= mask)
+    res = cv2.bitwise_and(img,img, mask= mask)
     cv2.imwrite(output_path,res)
     return True
 
