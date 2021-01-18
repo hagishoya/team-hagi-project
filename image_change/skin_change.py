1a64c4d129bfd45426e87b4c01c91b4186cf151f
hagishoya <hagi19s022@gmail.com>
2021-01-18 01:45:37 +0000

    tuika

--- image_change/skin_change.py
+++ image_change/skin_change.py
@@ -14,16 +14,26 @@ import random
 
 def skin_image(event,userid,color):
     image_path, output_path = path_data.get_image_path(event,userid)
-  
+
  
-    image = cv2.imread(image_path)     # Load image
+    img = cv2.imread(image_path)     # Load image
  
 
  
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) # BGR->HSV変換
+    height = img.shape[0]
+    width = img.shape[1]
+    img2 = cv2.resize(img , (int(width*0.5), int(height*0.5)))
+    hsv = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV) # BGR->HSV変換
     hsv_2 = np.copy(hsv)
-
-    #a = random.randint(1,6)
+    #hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>5) & (hsv[:, :, 0]<30) ,hsv[:, :, 0] + 150,hsv[:, :, 0])
+    #hsv_2[:, :, 2] = np.where((hsv[:, :, 2]>5) )
+    #hsv_2[:, :, 2] = np.where((hsv[:, :, 0]>5) & (hsv[:, :, 0]<30) ,hsv[:, :, 2] *0.5,hsv[:, :, 2])#金髪から黒髪
+    #hsv_2[:, :, 2] = np.where((hsv[:, :, 0]>5) & (hsv[:, :, 0]<30) ,hsv[:, :, 2] *0.5,hsv[:, :, 2])
+    #hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>0) & (hsv[:, :, 2]<128) ,hsv[:, :, 0] +110 ,hsv[:, :, 0])#黒(茶色)から青
+    #hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>0) & (hsv[:, :, 2]<128) ,hsv[:, :, 0] +50 ,hsv[:, :, 0])#黒(茶色)から緑
+    #hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>0) & (hsv[:, :, 2]<128) ,hsv[:, :, 0] +160 ,hsv[:, :, 0])#黒(茶色)から赤
+    #bgr = cv2.cvtColor(hsv_2, cv2.COLOR_HSV2BGR)
+    #cv2.imwrite(output_path,bgr)
 
 
     if color == 1:
@@ -41,7 +51,7 @@ def skin_image(event,userid,color):
     #hsv_2[:, :, 2] = np.where((hsv_2[:, :, 0]>6) & (hsv_2[:, :, 0]<30) ,hsv_2[:, :, 1] *0.7,hsv_2[:, :, 2]) #黒色
     #0.001 赤
     #0.3 緑
-    bgr = cv2.cvtColor(hsv_2, cv2.COLOR_HSV2BGR)
+    #bgr = cv2.cvtColor(hsv_2, cv2.COLOR_HSV2BGR)
     
 
     bgr = cv2.cvtColor(hsv_2, cv2.COLOR_HSV2BGR)
