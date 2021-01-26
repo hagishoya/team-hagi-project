e7f69b655b17d6d76c9a636ab3e70f23166182e2
hagishoya <hagi19s022@gmail.com>
2021-01-26 01:51:17 +0000

    main髪の毛用

--- image_change/hair_change.py
+++ image_change/hair_change.py
@@ -12,152 +12,125 @@ import numpy as np
 import path_data
 
 
-def get_head_mask(img):
- 
 
-    cascadePath = "haarcascade_frontalface_default.xml"
-    faceCascade = cv2.CascadeClassifier(cascadePath)
-
-    """
-    The algorithm works best when a person is on a background that does not merge with his hair. Also, the hair should not merge with the complexion.
-    """
-    """
-    Get the mask of the head
-    Cuting  BG
-    :param img: source image
-    :return:   Returns the mask with the cut out BG
-    """
-    mask = np.zeros(img.shape[:2], np.uint8)
-    bgdModel = np.zeros((1, 65), np.float64)
-    fgdModel = np.zeros((1, 65), np.float64)
-    faces = faceCascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5, minSize=(50, 50))    # Find faces
-    if len(faces) != 0:
-        x, y, w, h = faces[0]
-        (x, y, w, h) = (x - 40, y - 100, w + 80, h + 200)
-        rect1 = (x, y, w, h)
-        cv2.grabCut(img, mask, rect1, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)     #Crop BG around the head
-    mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')  # Take the mask from BG
-
-    return mask2
-
-def is_bold(pnt, hair_mask):
-    """
-    Check band or not
-    :param pnt: The upper point of the head
-    :param hair_mask: Mask with hair
-    :return: True if Bald, else False
-    """
-    roi = hair_mask[pnt[1]:pnt[1] + 40, pnt[0] - 40:pnt[0] + 40]    # Select the rectangle under the top dot
-    cnt = cv2.countNonZero(roi) # Count the number of non-zero points in this rectangle
-    # If the number of points is less than 25%, then we think that the head is bald
-    if cnt < 800:
-        print("Bald human on phoro")
-        return True
-    else:
-        print("Not Bold")
-        return False
-
-#def hsv_to_rgb(h,s,v):
-##    h = 0 ~ 360
-##    s = 0 ~ 255
-##    v = 0 ~ 255
-#    i = int(h / 60.0)
-#    mx = v
-#    mn = v - ((s / 255.0) * v)
-#    if h is None:
-#        return(0,0,0)
-#    if i == 0:
-#        (r1,g1,b1) = (mx,(h/60.0)*(mx-mn)+mn,mn)
-#    elif i == 1:
-#        (r1,g1,b1) = (((120.0-h)/60.0)*(mx-mn)+mn,mx,mn)
-#    elif i == 2:
-#        (r1,g1,b1) = (mn,mx,((h-120.0)/60.0)*(mx-mn)+mn)
-#    elif i == 3:
-#        (r1,g1,b1) = (mn,((240.0-h)/60.0)*(mx-mn)+mn,mx)
-#    elif i == 4:
-#        (r1,g1,b1) = (((h-240.0)/60.0)*(mx-mn)+mn,mn,mx)
-#    elif 5 <= i:
-#        (r1,g1,b1) = (mx,mn,((360.0-h)/60.0)*(mx-mn)+mn)
-#    return (int(r1), int(g1), int(b1))
-
-def hair_image(event,userid):
+def hair_image(event,userid,color):
     image_path, output_path = path_data.get_image_path(event,userid)
 
-    image = cv2.imread(image_path)     # Load image
-    image = imutils.resize(image, height=500)     # We result in 500px in height
-    mask = get_head_mask(image)      # We get the mask of the head (without BG))
 
-    # Find the contours, take the largest one and memorize its upper point as the top of the head
-    cnts = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[0]
-    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-    cnt=cnts[0]
-    topmost = tuple(cnt[cnt[:,:,1].argmin()][0])
+    img = cv2.imread(image_path)     # Load image
+ 
 
+ 
+    #height = img.shape[0]
+    #width = img.shape[1]
+    #img2 = cv2.resize(img , (int(width*0.5), int(height*0.5)))
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) # BGR->HSV変換
+    hsv_2 = np.copy(hsv)
+    hsv_3 = np.copy(hsv_2)
+    hsv_4 = np.copy(hsv_3)
+    hsv_5 = np.copy(hsv_4)
+    hsv_6 = np.copy(hsv_5)
+    hsv_7 = np.copy(hsv_6)
+    hsv_8 = np.copy(hsv_7)
+    hsv_9 = np.copy(hsv_8)
+    hsv_10 = np.copy(hsv_9)
+    #hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>5) & (hsv[:, :, 0]<30) ,hsv[:, :, 0] + 150,hsv[:, :, 0])
+    #hsv_2[:, :, 2] = np.where((hsv[:, :, 2]>5) )
+    #hsv_2[:, :, 2] = np.where((hsv[:, :, 0]>5) & (hsv[:, :, 0]<30) ,hsv[:, :, 2] *0.5,hsv[:, :, 2])#金髪から黒髪
+    #hsv_2[:, :, 2] = np.where((hsv[:, :, 0]>5) & (hsv[:, :, 0]<30) ,hsv[:, :, 2] *0.5,hsv[:, :, 2])
+    #hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>0) & (hsv[:, :, 2]<128) ,hsv[:, :, 0] +110 ,hsv[:, :, 0])#黒(茶色)から青
+    #hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>0) & (hsv[:, :, 2]<128) ,hsv[:, :, 0] +50 ,hsv[:, :, 0])#黒(茶色)から緑
+    #hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>0) & (hsv[:, :, 2]<128) ,hsv[:, :, 0] +160 ,hsv[:, :, 0])#黒(茶色)から赤
+    #bgr = cv2.cvtColor(hsv_2, cv2.COLOR_HSV2BGR)
+    #cv2.imwrite(output_path,bgr)
+    #hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>0) & (hsv[:, :, 2]<128) ,hsv[:, :, 0] +50 ,hsv[:, :, 0])
+    if color == 1:
+        #色相を０に
+        #hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>=0) & (hsv[:, :, 2]<30) ,hsv[:, :, 0] *0.001 ,hsv[:, :, 0])
+        #hsv_3[:, :, 0] = np.where((hsv_2[:, :, 2]>30) & (hsv_2[:, :, 2]<60) ,hsv_2[:, :, 0] *0.001 ,hsv_2[:, :, 0])#150~180
+        #hsv_4[:, :, 0] = np.where((hsv_3[:, :, 2]>60) & (hsv_3[:, :, 2]<90) ,hsv_3[:, :, 0] *0.001 ,hsv_3[:, :, 0])#160~190
+        #明度をあげる
+        ##hsv_5[:, :, 2] = np.where((hsv_4[:, :, 2]>=0) & (hsv_4[:, :, 2]<30) ,hsv_4[:, :, 2] + 140 ,hsv_4[:, :, 2])#140~170
+        ##hsv_6[:, :, 2] = np.where((hsv_5[:, :, 2]>30) & (hsv_5[:, :, 2]<60) ,hsv_5[:, :, 2] + 120 ,hsv_5[:, :, 2])#150~180
+        ##hsv_7[:, :, 2] = np.where((hsv_6[:, :, 2]>60) & (hsv_6[:, :, 2]<90) ,hsv_6[:, :, 2] + 100 ,hsv_6[:, :, 2])#160~190
+#ここから
+        #彩度を上げる
+        hsv_5[:, :, 1] = np.where((hsv_4[:, :, 2]>=0) & (hsv_4[:, :, 2]<30) ,hsv_4[:, :, 1] + 230 ,hsv_4[:, :, 1])
+        #hsv_6[:, :, 1] = np.where((hsv_5[:, :, 2]>30) & (hsv_5[:, :, 2]<60) ,hsv_5[:, :, 1] + 230 ,hsv_5[:, :, 1])
+        #hsv_7[:, :, 1] = np.where((hsv_6[:, :, 2]>60) & (hsv_6[:, :, 2]<100) ,hsv_6[:, :, 1] + 230 ,hsv_6[:, :, 1])
 
-    # We remove the face by the color of the skin
-    lower = np.array([0, 0, 100], dtype="uint8")  # Lower limit of skin color
-    upper = np.array([255, 255, 255], dtype="uint8")  # Upper skin color limit
-    converted = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)   # We translate into HSV color format
-    skinMask = cv2.inRange(converted, lower, upper)     # Write a mask from places where the color is between the outside
-    mask[skinMask == 255] = 0   # We remove the face mask from the mask of the head
-
-    kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    mask = cv2.dilate(mask, kernel1, iterations=1)
-    i1 = cv2.bitwise_and(image, image, mask=mask)
-
-
-    # 髪の毛なし
-    if is_bold(topmost,mask):
-        cv2.rectangle(image,topmost,topmost,(0,0,255),5)
-        print(topmost)
-
-
-
-    # 髪の毛あり
-    else:
-        #輪郭取得
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
-        cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-        cv2.drawContours(image,[cnts[0]],-1,(0,0,255),2)
-       # green = np.uint8([[[0,255,0 ]]])
-        #bgr = cv2.cvtColor(np.array([[[0,255,0]]], dtype=np.uint8), cv2.COLOR_HSV2BGR)[0][0]
-        #ポリゴンの領域を塗りつぶす
         
-        #test_color = hsv_to_rgb(300, 200 , 200)
-        # 色基準で2値化する。
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-        # 色の範囲を指定する
-        lower_color = np.array([0, 0, 0])
-        upper_color = np.array([255, 255, 255])
-
-        # 指定した色に基づいたマスク画像の生成
-        mask = cv2.inRange(hsv, lower_color, upper_color)
-        output = cv2.bitwise_and(hsv, hsv, mask = mask)
-        print(output)
-        print("\n\n\n\n\n\n\n\n\n")
-        bgr_output = colorsys.hsv_to_rgb(output)
-        bgr_color = bgr_output + (0,0,80)
-        cv2.fillPoly(image, pts =[cnts[0]], color= bgr_color)
-        #cv2.fillPoly(image, pts =[cnts[0]], color= (255,0,0))
-        #green = np.uint8([[[0,255,0 ]]])
-        #hsv_green = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
-        #image[:] = hsv_green
-        for c in cnts[0]:
-            print(c)
 
 
-    if bool:
-        # 認識結果の保存
-        cv2.imwrite(output_path, image)
-        #cv2.imwrite(output_path2, image)
-        return True
-    else:
-        return False
-    # # Display the image in a loop
-    # while True:
-    #     cv2.imwrite(output_path, image)
-    #     #cv2.imshow("image1", image)
-    #     # Exit to Esc
-    #     if cv2.waitKey(5) == 27:
-    #         break
+        #色相を上げる
+        hsv_8[:, :, 0] = np.where((hsv_5[:, :, 2]>=0) & (hsv_5[:, :, 2]<30) ,hsv_5[:, :, 0] +80 ,hsv_5[:, :, 0])
+        #hsv_9[:, :, 0] = np.where((hsv_8[:, :, 2]>30) & (hsv_8[:, :, 2]<60) ,hsv_8[:, :, 0] +80 ,hsv_8[:, :, 0])#150~180
+        #hsv_10[:, :, 0] = np.where((hsv_9[:, :, 2]>60) & (hsv_9[:, :, 2]<100) ,hsv_9[:, :, 0]+80 ,hsv_9[:, :, 0])#160~190
+
+
+        #明度を上げる
+        hsv_11 = np.copy(hsv_8)
+        hsv_11[:, :, 2] = np.where((hsv_8[:, :, 2]>=0) & (hsv_8[:, :, 2]<30) ,hsv_8[:, :, 2] + 180 ,hsv_8[:, :, 2])#140~170
+        
+        hsv_12 = np.copy(hsv_11)
+        #hsv_12[:, :, 2] = np.where((hsv_11[:, :, 2]>30) & (hsv_11[:, :, 2]<60) ,hsv_11[:, :, 2] + 160 ,hsv_11[:, :, 2])#150~180
+
+        hsv_13 = np.copy(hsv_12)
+        #hsv_13[:, :, 2] = np.where((hsv_12[:, :, 2]>60) & (hsv_12[:, :, 2]<100) ,hsv_12[:, :, 2] + 140 ,hsv_12[:, :, 2])#160~190
+#ここまで
+        #色相追加
+        #彩度をあげる
+        ##hsv_8[:, :, 1] = np.where((hsv_7[:, :, 2]>140) & (hsv_7[:, :, 2]<190) & (hsv_4[:, :, 2]>=0) & (hsv_4[:, :, 2]<30) ,hsv_7[:, :, 1] + 100 ,hsv_7[:, :, 1])
+        #hsv_8[:, :, 1] = np.where((hsv_7[:, :, 2]>140) & (hsv_7[:, :, 2]<190) & (hsv_7[:, :, 1]<100) ,hsv_7[:, :, 1] + 100 ,hsv_7[:, :, 1])#& (hsv_4[:, :, 1]<20) 
+        #hsv_9[:, :, 1] = np.where(((hsv_8[:, :, 2]>150) & (hsv_8[:, :, 2]<180)) & (hsv_8[:, :, 1]<100) ,hsv_8[:, :, 1] + 100,hsv_8[:, :, 1])
+        #hsv_10[:, :, 1] = np.where(((hsv_9[:, :, 2]>160) & (hsv_9[:, :, 2]<190)) & (hsv_9[:, :, 1]<100) ,hsv_9[:, :, 1] + 100 ,hsv_9[:, :, 1])
+        #hsv_5[:, :, 2] = np.where((hsv_4[:, :, 2]>90) & (hsv_4[:, :, 2]<120) ,hsv_4[:, :, 2] + 80 ,hsv_4[:, :, 2])
+        #hsv_3[:, :, 1] = np.where(((hsv[:, :, 2]>30) & (hsv[:, :, 2]<128)) & (hsv[:, :, 1]<100) ,hsv_2[:, :, 1] + 100 ,hsv_2[:, :, 1],)
+        #hsv_3[:, :, 2] = np.where(((hsv_2[:, :, 2]>0) & (hsv_2[:, :, 2]<200)) & (hsv_2[:, :, 1]<255) ,hsv_2[:, :, 1] -190 ,hsv_2[:, :, 2])
+        #hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>6) & (hsv[:, :, 0]<30) ,hsv[:, :, 0] + 50,hsv[:, :, 0]) #緑
+    elif color == 2:
+        hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>6) & (hsv[:, :, 2]<128) ,hsv[:, :, 0] + 110,hsv[:, :, 0]) #青
+    elif color == 3:
+        hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>6) & (hsv[:, :, 2]<128) ,hsv[:, :, 0] + 20,hsv[:, :, 0]) #黄色
+    elif color == 4:
+        hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>0) & (hsv[:, :, 2]<128) ,hsv[:, :, 0] + 176,hsv[:, :, 0]) #ピンク
+        hsv_2[:, :, 1] = np.where((hsv[:, :, 0]>=176) ,hsv[:, :, 1] + 96 ,hsv[:, :, 1])
+    elif color == 5:
+        hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>6) & (hsv[:, :, 2]<128) ,hsv[:, :, 0] *0.001,hsv[:, :, 0]) #赤色
+    elif color == 6:
+        hsv_2[:, :, 2] = np.where((hsv[:, :, 0]>6) & (hsv[:, :, 0]<30) ,hsv[:, :, 2] *0.4,hsv[:, :, 2]) #黒色
+
+
+
+
+    #茶色から指定色-----------------------------------------------------------------------------------------------------------
+    #if color == 1:
+        #hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>0) & (hsv[:, :, 2]<255) ,hsv[:, :, 0] +50 ,hsv[:, :, 0])#黒(茶色)から緑
+        #hsv_2[:, :, 0] = np.where((hsv[:, :, 0]>6) & (hsv[:, :, 0]<30) ,hsv[:, :, 0] + 50,hsv[:, :, 0]) #緑
+    #elif color == 2:
+        #hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>6) & (hsv[:, :, 2]<128) ,hsv[:, :, 0] + 110,hsv[:, :, 0]) #青
+    #elif color == 3:
+        #hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>6) & (hsv[:, :, 2]<128) ,hsv[:, :, 0] + 20,hsv[:, :, 0]) #黄色
+    #elif color == 4:
+        #hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>0) & (hsv[:, :, 2]<128) ,hsv[:, :, 0] + 176,hsv[:, :, 0]) #ピンク
+        #hsv_2[:, :, 1] = np.where((hsv[:, :, 0]>=176) ,hsv[:, :, 1] + 96 ,hsv[:, :, 1])
+    #elif color == 5:
+        #hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>6) & (hsv[:, :, 2]<128) ,hsv[:, :, 0] *0.001,hsv[:, :, 0]) #赤色
+    #elif color == 6:
+        #hsv_2[:, :, 2] = np.where((hsv[:, :, 0]>6) & (hsv[:, :, 0]<30) ,hsv[:, :, 2] *0.4,hsv[:, :, 2]) #黒色
+    #hsv_2[:, :, 2] = np.where((hsv_2[:, :, 0]>6) & (hsv_2[:, :, 0]<30) ,hsv_2[:, :, 1] *0.7,hsv_2[:, :, 2]) #黒色
+    #0.001 赤
+    #0.3 緑
+    #bgr = cv2.cvtColor(hsv_2, cv2.COLOR_HSV2BGR)
+    #----------------------------------------------------------------------------------------------------------------------
+
+    bgr = cv2.cvtColor(hsv_11, cv2.COLOR_HSV2BGR)
+    cv2.imwrite(output_path, bgr)
+
+#hsv_2[:, :, 0] = np.where((hsv[:, :, 2]>0) & (hsv[:, :, 2]<128) ,hsv[:, :, 0] + 176,hsv[:, :, 0]) #濃い茶色
+#hsv_2[:, :, 1] = np.where((hsv[:, :, 2]>0) & (hsv[:, :, 2]<128) ,hsv[:, :, 1] + 96 ,hsv[:, :, 1])
+
+
+#白
+#hsv_2[:, :, 1] = np.where((hsv[:, :, 2]>0) & (hsv[:, :, 2]<128) ,hsv[:, :, 1] + 180 ,hsv[:, :, 1])
+#hsv_3[:, :, 2] = np.where((hsv_2[:, :, 2]>0) & (hsv_2[:, :, 2]<128) ,hsv_2[:, :, 2] + 180 ,hsv_2[:, :, 2])
---
+++ main_1_26.py
@@ -0,0 +1,351 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, ImageMessage, FlexSendMessage,CarouselContainer,BubbleContainer
+from image_change import mosic_change, art_change, dot_change, illust_change, hair_change, skin_change
+from output import output_method
+import json
+import os
+import imutils
+import matplotlib.pyplot as plt
+import numpy as np
+
+# リプライIDとイベントIDをテキストとして保存するためのやつ
+work = {}
+path_w1 = 'saveid.txt'
+path_w2 = 'savereply.txt'
+app = Flask(__name__)
+
+# トークン情報もろもろ
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ['YOUR_CHANNEL_ACCESS_TOKEN']
+YOUR_CHANNEL_SECRET = os.environ['YOUR_CHANNEL_SECRET']
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+FQDN = os.environ['FQDN']
+cascade_path = "haarcascade_frontalface_default.xml"
+cascade_eye_path = "haarcascade_eye.xml"
+
+
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+
+    body = request.get_data(as_text=True)
+    print("Request body" + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return "OK"
+#-----------
+#flex初期位置
+#-----------
+    # message = []
+    # work = event.message.id
+    # reply_work = event.reply_token
+    # print("取得イヴェントメッセージIDDDDDDDDDDDDDDDD:{}".format(work))
+    # text_save_id(work)
+    # text_save_reply(reply_work)
+
+    # # Json展開
+    # json_open = open('test.json', 'r')
+    # json_data = json.load(json_open)
+    # if event.reply_token == "00000000000000000000000000000000":
+    #     return
+    # if event.reply_token == "ffffffffffffffffffffffffffffffff":
+    #     return
+    
+    # line_bot_api.reply_message(event.reply_token, message) 
+
+
+# テキストデータを受け取ったときに走るやつ。
+@handler.add(MessageEvent, message=TextMessage)
+
+
+def handle_message(event):
+    print("取得イヴェント:{}".format(event))
+    print("取得イヴェントメッセージID:{}".format(event.message.id))
+    print("リプライトークン：{}".format(event.reply_token))
+    print("------リプライ型------")
+    print(type(event.reply_token))
+
+    profile = line_bot_api.get_profile(event.source.user_id)
+    print("ユーザーID：{}".format(profile.user_id))
+
+    userId = profile.user_id
+
+
+    #モザイク(目)
+    if event.message.text == ">>おめめモザイク" and os.path.exists("static/" + userId):
+        print("通過: {}".format(event.message.text))
+        with open(path_w1) as f:
+            work = f.read()
+        with open(path_w2) as f2:
+            work1 = f2.read()
+        output_method.handle_send_message(work,event.reply_token,userId)
+
+    #線画
+    elif event.message.text == ">>線画風" and os.path.exists("static/" + userId):
+        print("通過: {}".format(event.message.text))
+        with open(path_w1) as f:
+            work = f.read()
+        with open(path_w2) as f2:
+            work1 = f2.read()
+        output_method.handle_send_message2(work,event.reply_token,userId)
+
+    #イラスト風
+    elif event.message.text == ">>イラスト風" and os.path.exists("static/" + userId):
+        print("通過: {}".format(event.message.text))
+        with open(path_w1) as f:
+            work = f.read()
+        with open(path_w2) as f2:
+            work1 = f2.read()
+        output_method.handle_send_message3(work,event.reply_token,userId)
+
+    #ドット絵
+    elif event.message.text == ">>ドット絵風" and os.path.exists("static/" + userId):
+        print("通過: {}".format(event.message.text))
+        with open(path_w1) as f:
+            work = f.read()
+        with open(path_w2) as f2:
+            work1 = f2.read()
+        output_method.handle_send_message4(work,event.reply_token,userId)
+    
+#----------------------------------------------------------------------------------------------------------
+    #髪の毛test
+    elif event.message.text == ">>髪色変更" and os.path.exists("static/" + userId):
+        print("通過: {}".format(event.message.text))
+        #with open(path_w1) as f:
+        #    work = f.read()
+        #with open(path_w2) as f2:
+        #    work1 = f2.read()
+        #output_method.handle_send_message5(work,event.reply_token,userId)
+        flex2(event)
+
+    elif event.message.text == ">>黒色" and os.path.exists("static/" + userId):
+        print("通過: {}".format(event.message.text))
+        #with open(path_w1) as f:
+        #    work = f.read()
+        #with open(path_w2) as f2:
+        #    work1 = f2.read()
+        #output_method.handle_send_message5(work,event.reply_token,userId)
+        flex3(event)
+    
+
+#----------------------------------------------------------------------------------------------------------
+
+    #肌の色
+    elif event.message.text == ">>肌変更" and os.path.exists("static/" + userId):
+        print("通過: {}".format(event.message.text))
+        # with open(path_w1) as f:
+        #     work = f.read()
+        # with open(path_w2) as f2:
+        #     work1 = f2.read()
+        #output_method.handle_send_message6(work,event.reply_token,userId)
+
+        flex(event)
+
+    elif event.message.text == ">>緑色変更" and os.path.exists("static/" + userId):
+        print("「緑」通過: {}".format(event.message.text))
+
+        color = 1
+        with open(path_w1) as f:
+            work = f.read()
+        with open(path_w2) as f2:
+            work1 = f2.read()
+        output_method.handle_send_message6(work,event.reply_token,userId,color)
+
+    elif event.message.text == ">>青色変更" and os.path.exists("static/" + userId):
+        print("通過: {}".format(event.message.text))
+
+        color = 2
+        with open(path_w1) as f:
+            work = f.read()
+        with open(path_w2) as f2:
+            work1 = f2.read()
+        output_method.handle_send_message6(work,event.reply_token,userId,color)
+
+    elif event.message.text == ">>黄色変更" and os.path.exists("static/" + userId):
+        print("通過: {}".format(event.message.text))
+
+        color = 3
+        with open(path_w1) as f:
+            work = f.read()
+        with open(path_w2) as f2:
+            work1 = f2.read()
+        output_method.handle_send_message6(work,event.reply_token,userId,color)
+
+    elif event.message.text == ">>ピンク変更" and os.path.exists("static/" + userId):
+        print("通過: {}".format(event.message.text))
+
+        color = 4
+        with open(path_w1) as f:
+            work = f.read()
+        with open(path_w2) as f2:
+            work1 = f2.read()
+        output_method.handle_send_message6(work,event.reply_token,userId,color)
+
+    elif event.message.text == ">>赤色変更" and os.path.exists("static/" + userId):
+        print("通過: {}".format(event.message.text))
+
+        color = 5
+        with open(path_w1) as f:
+            work = f.read()
+        with open(path_w2) as f2:
+            work1 = f2.read()
+        output_method.handle_send_message6(work,event.reply_token,userId,color)
+
+    elif event.message.text == ">>黒色変更" and os.path.exists("static/" + userId):
+        print("通過: {}".format(event.message.text))
+
+        color = 6
+        with open(path_w1) as f:
+            work = f.read()
+        with open(path_w2) as f2:
+            work1 = f2.read()
+        output_method.handle_send_message6(work,event.reply_token,userId,color)
+
+
+def flex(event):
+    message = []
+    work = event.message.id
+    reply_work = event.reply_token
+    print("取得イヴェントメッセージIDDDDDDDDDDDDDDDD:{}".format(work))
+    #text_save_id(work)
+    #text_save_reply(reply_work)
+    json_open = open('test.json', 'r')
+    json_data = json.load(json_open)
+    user_id = os.environ["USER_ID"]
+    
+    
+    messages = FlexSendMessage(alt_text="test", contents=json_data)
+    print("フレックスメッセージ中身: {}".format(messages))
+    if event.reply_token == "00000000000000000000000000000000":
+        return
+    if event.reply_token == "ffffffffffffffffffffffffffffffff":
+        return
+    
+    line_bot_api.reply_message(event.reply_token, messages)
+
+
+def flex2(event):
+    message = []
+    work = event.message.id
+    reply_work = event.reply_token
+    print("取得イヴェントメッセージIDDDDDDDDDDDDDDDD:{}".format(work))
+    json_open = open('hair_change_myself.json', 'r')
+    json_data = json.load(json_open)
+    user_id = os.environ["USER_ID"]
+    
+    messages = FlexSendMessage(alt_text="test", contents=json_data)
+    print("フレックスメッセージ中身: {}".format(messages))
+    if event.reply_token == "00000000000000000000000000000000":
+        return
+    if event.reply_token == "ffffffffffffffffffffffffffffffff":
+        return
+    
+    line_bot_api.reply_message(event.reply_token, messages)
+
+
+def flex3(event):
+    message = []
+    print("取得イヴェントメッセージIDDDDDDDDDDDDDDDD:{}".format(work))
+    json_open = open('hair_change.json', 'r')
+    json_data = json.load(json_open)
+    user_id = os.environ["USER_ID"]
+
+    messages = FlexSendMessage(alt_text="test", contents=json_data)
+    print("フレックスメッセージ中身: {}".format(messages))
+    if event.reply_token == "00000000000000000000000000000000":
+        return
+    if event.reply_token == "ffffffffffffffffffffffffffffffff":
+        return
+    
+    line_bot_api.reply_message(event.reply_token, messages)
+    
+
+
+def text_save_id(work):
+    s = work
+    print("取得イヴェントメッセージIDDDDDDDDDDDDDDDD_text_saveID:{}".format(work))
+    with open(path_w1, mode='w') as f:
+        f.write(s)
+
+
+
+def text_save_reply(work):
+    s = work
+    print("取得イヴェントメッセージIDDDDDDDDDDDDDDDD_text_saveReply:{}".format(work))
+    with open(path_w2, mode='w') as f:
+        f.write(s)
+
+
+def carousel(event):
+    message = []
+    work = event.message.id
+    reply_work = event.reply_token
+    print("取得イヴェントメッセージIDDDDDDDDDDDDDDDD:{}".format(work))
+    text_save_id(work)
+    text_save_reply(reply_work)
+
+    # Json展開
+    json_open = open('carousel.json', 'r')
+    json_data = json.load(json_open)
+
+    message.append(TextSendMessage(text = "メニューを選択してね"))
+    message.append(FlexSendMessage(alt_text="test", contents=json_data))
+
+    if event.reply_token == "00000000000000000000000000000000":
+        return
+    if event.reply_token == "ffffffffffffffffffffffffffffffff":
+        return
+    
+    line_bot_api.reply_message(event.reply_token, message)   
+
+
+def carousel_skin(event):
+    message = []
+    work = event.message.id
+    reply_work = event.reply_token
+    print("取得イヴェントメッセージIDDDDDDDDDDDDDDDD:{}".format(work))
+    text_save_id(work)
+    text_save_reply(reply_work)
+
+    # Json展開
+    json_open = open('carousel_skin.json', 'r')
+    json_data = json.load(json_open)
+
+    message.append(TextSendMessage(text = "変更したい色を選択してね"))
+    message.append(FlexSendMessage(alt_text="test", contents=json_data))
+
+    if event.reply_token == "00000000000000000000000000000000":
+        return
+    if event.reply_token == "ffffffffffffffffffffffffffffffff":
+        return
+    
+    line_bot_api.reply_message(event.reply_token, message)   
+
+#画像受信後処理
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image_message(event):
+    print("メッセージID")
+    print(event.message.id)
+    message_content = line_bot_api.get_message_content(event.message.id)
+    profile = line_bot_api.get_profile(event.source.user_id)
+    print("ユーザーID：{}".format(profile.user_id))
+
+    userId = profile.user_id
+
+    if not os.path.exists("static/" + userId):
+        os.makedirs("static/" + userId)
+    with open("static/" + userId + '/' + event.message.id + ".jpg", "wb") as f:
+        f.write(message_content.content)
+    print(FQDN + "/static/"+ userId + "/" + event.message.id + ".jpg")
+    carousel(event)
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
